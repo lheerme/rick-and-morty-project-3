5c6d2f160800a319b2e0b93d616a7d30ee0bf94b
Guilherme Souza <gsouza2651@gmail.com>
2025-03-07 15:18:46 -0300

    feat: add Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
-export default function Home() {
+import homeLogo from '@/assets/home-logo.png'
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default async function Home() {
   return (
-    <main>
-      <div>Hello world!</div>
-    </main>
+    <div className="flex flex-col md:flex-row items-center justify-center md:justify-around m-auto h-full gap-4 px-6 py-4">
+      <Image
+        src={homeLogo}
+        className="w-full max-w-[200px] md:max-w-xs h-auto"
+        alt="rick and morty cromulons floating head"
+        placeholder="blur"
+      />
+      <div className="max-w-md w-full flex flex-col items-center gap-5">
+        <p className="text-center text-xl">
+          Immerse yourself in the Rick and Morty multiverse with real-time data
+          using the Rick and Morty API. Explore characters, episodes, and
+          locations!
+        </p>
+        <Button asChild>
+          <Link href="/characters">Begin</Link>
+        </Button>
+      </div>
+    </div>
   )
 }
