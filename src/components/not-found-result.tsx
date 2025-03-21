d1b91929480d56cb90cc38b0e6462f9317f78317
Guilherme Souza <gsouza2651@gmail.com>
2025-03-21 14:16:19 -0300

    chore: add placeholder="blur" to NotFoundResult Image

--- src/components/not-found-result.tsx
+++ src/components/not-found-result.tsx
@@ -8,7 +8,7 @@ interface NotFoundResultProps {
 export function NotFoundResult({ text = 'Not found :/' }: NotFoundResultProps) {
   return (
     <div className="w-full max-w-[250px] space-y-4 m-auto">
-      <Image src={notFoundImage} alt="" className="w-full" />
+      <Image src={notFoundImage} alt="" className="w-full" placeholder="blur" />
       <p className="text-lg text-center">{text}</p>
     </div>
   )
